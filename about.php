6bc143a81821b8903aa2ecef5bd85ea16a2ce4c9
Farren Lyrananda Rachman <116480927+cherrysober@users.noreply.github.com>
2024-04-04 23:30:48 +0800

    Update about.php

--- about.php
+++ about.php
@@ -325,58 +325,58 @@
     </section>
 
     <footer class="ftco-footer">
-		<div class="container">
-		  <div class="row mb-5">
-			<div class="col-sm-12 col-md">
-			  <div class="ftco-footer-widget mb-4">
-				<h2 class="ftco-heading-2 logo"><a href="#">Candle <span>Store</span></a></h2>
-				<p>In a world where chaos reigns, find solace in the gentle flicker and comforting scents of Candle Store candles.</p>
-				<ul class="ftco-footer-social list-unstyled mt-2">
-				  <li class="ftco-animate"><a href="#"><span class="fa fa-twitter"></span></a></li>
-				  <li class="ftco-animate"><a href="#"><span class="fa fa-facebook"></span></a></li>
-				  <li class="ftco-animate"><a href="#"><span class="fa fa-instagram"></span></a></li>
-				</ul>
-			  </div>
-			</div>
-			<div class="col-sm-12 col-md">
-			  <div class="ftco-footer-widget mb-4 ml-md-4">
-				<h2 class="ftco-heading-2">My Accounts</h2>
-				<ul class="list-unstyled">
-				  <li><a href="account.php"><span class="fa fa-chevron-right mr-2"></span>My Account</a></li>
-				  <li><a href="register.php"><span class="fa fa-chevron-right mr-2"></span>Register</a></li>
-				  <li><a href="login.php"><span class="fa fa-chevron-right mr-2"></span>Log In</a></li>
-				  <li><a href="cart.php"><span class="fa fa-chevron-right mr-2"></span>My Cart</a></li>
-				</ul>
-			  </div>
-			</div>
-			<div class="col-sm-12 col-md">
-			  <div class="ftco-footer-widget mb-4 ml-md-4">
-				<h2 class="ftco-heading-2">Information</h2>
-				<ul class="list-unstyled">
-				  <li><a href="#"><span class="fa fa-chevron-right mr-2"></span>About us</a></li>
-				  <li><a href="#"><span class="fa fa-chevron-right mr-2"></span>Catalog</a></li>
-				  <li><a href="#"><span class="fa fa-chevron-right mr-2"></span>Contact us</a></li>
-				</ul>
-			  </div>
-			</div>
-			<div class="col-sm-12 col-md">
-			  <div class="ftco-footer-widget mb-4">
-				  <h2 class="ftco-heading-2">Have a Questions?</h2>
-				  <div class="block-23 mb-3">
-					<ul>
-					  <li><span class="icon fa fa-map marker"></span><span class="text">203 Fake St. Mountain View, San Francisco, California, USA</span></li>
-					  <li><a href="#"><span class="icon fa fa-phone"></span><span class="text">0812345618</span></a></li>
-					  <li><a href="#"><span class="icon fa fa-paper-plane pr-4"></span><span class="text">candlestore@gmail.com</span></a></li>
-					</ul>
-				  </div>
-			  </div>
-			</div>
-		  </div>
-		</div>
-		<div class="container-fluid px-0 py-5 bg-black">
-			<div class="container">
-				<div class="row">
-				<div class="col-md-12">
+      <div class="container">
+        <div class="row mb-5">
+          <div class="col-sm-12 col-md">
+            <div class="ftco-footer-widget mb-4">
+              <h2 class="ftco-heading-2 logo"><a href="#">Candle <span>Store</span></a></h2>
+              <p>In a world where chaos reigns, find solace in the gentle flicker and comforting scents of Candle Store candles.</p>
+              <ul class="ftco-footer-social list-unstyled mt-2">
+                <li class="ftco-animate"><a href="#"><span class="fa fa-twitter"></span></a></li>
+                <li class="ftco-animate"><a href="#"><span class="fa fa-facebook"></span></a></li>
+                <li class="ftco-animate"><a href="#"><span class="fa fa-instagram"></span></a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-sm-12 col-md">
+            <div class="ftco-footer-widget mb-4 ml-md-4">
+              <h2 class="ftco-heading-2">My Accounts</h2>
+              <ul class="list-unstyled">
+                <li><a href="account.php"><span class="fa fa-chevron-right mr-2"></span>My Account</a></li>
+                <li><a href="register.php"><span class="fa fa-chevron-right mr-2"></span>Register</a></li>
+                <li><a href="login.php"><span class="fa fa-chevron-right mr-2"></span>Log In</a></li>
+                <li><a href="cart.php"><span class="fa fa-chevron-right mr-2"></span>My Cart</a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-sm-12 col-md">
+            <div class="ftco-footer-widget mb-4 ml-md-4">
+              <h2 class="ftco-heading-2">Information</h2>
+              <ul class="list-unstyled">
+                <li><a href="about.php"><span class="fa fa-chevron-right mr-2"></span>About us</a></li>
+                <li><a href="product.php"><span class="fa fa-chevron-right mr-2"></span>Catalog</a></li>
+                <li><a href="contact.php"><span class="fa fa-chevron-right mr-2"></span>Contact us</a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-sm-12 col-md">
+            <div class="ftco-footer-widget mb-4">
+            	<h2 class="ftco-heading-2">Have a Questions?</h2>
+            	<div class="block-23 mb-3">
+	              <ul>
+	                <li><span class="icon fa fa-map marker"></span><span class="text">203 Fake St. Mountain View, San Francisco, California, USA</span></li>
+	                <li><a href="#"><span class="icon fa fa-phone"></span><span class="text">0812345618</span></a></li>
+	                <li><a href="#"><span class="icon fa fa-paper-plane pr-4"></span><span class="text">candlestore@gmail.com</span></a></li>
+	              </ul>
+	            </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="container-fluid px-0 py-5 bg-black">
+      	<div class="container">
+      		<div class="row">
+	          <div class="col-md-12">
 		  
 				  <p class="mb-0" style="color: rgba(255,255,255,.5);"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
 		Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart color-danger" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib.com</a>
