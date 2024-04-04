3be8a3090ee88b0d2a2a86e7cdcc13f46c1b7d4d
Farren Lyrananda Rachman <116480927+cherrysober@users.noreply.github.com>
2024-04-04 23:47:57 +0800

    Update checkout_proses.php

--- checkout_proses.php
+++ checkout_proses.php
@@ -8,16 +8,17 @@ $result = $con->query($sql);
 $carts = $result->fetch_all(MYSQLI_ASSOC);
 
 foreach ($carts as $cart) {
-	$product_id = $cart['id_products'];
-	$quantity = $cart['quantity'];
-	$item_price = $cart['price'];
-	$subtotal = $quantity * $item_price;
-	$sql = "INSERT INTO invoice (id_users, id_products, quantity, item_price, subtotal, invoice_date) VALUES ('$user_id', '$product_id', '$quantity', '$item_price', '$subtotal', NOW())";
-	$con->query($sql);
+    $product_id = $cart['id_products'];
+    $quantity = $cart['quantity'];
+    $item_price = $cart['price'];
+    $subtotal = $quantity * $item_price;
+    $sql = "INSERT INTO invoice (id_users, id_products, quantity, item_price, subtotal, invoice_date) VALUES ('$user_id', '$product_id', '$quantity', '$item_price', '$subtotal', NOW())";
+    $con->query($sql);
 }
 
 $sql = "DELETE FROM cart WHERE id_users = '$user_id' AND checked = 1";
 $con->query($sql);
 
 echo "<script>alert('Checkout berhasil!');</script>";
-echo "<script>location.href='cart.php';</script>";
+echo "<script>location.href='invoice.php';</script>"; 
+?>
